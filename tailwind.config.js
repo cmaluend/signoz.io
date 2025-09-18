920ff516e342fe0611219e25c0de26a8552ab209
manika-signoz <manika@signoz.io>
2025-09-18 15:13:45 +0530

    restore data folder in tw config, add tw classname to elb-logs for test

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
     './pages/**/*.{js,ts,tsx}',
     './components/**/*.{js,ts,tsx}',
     './layouts/**/*.{js,ts,tsx}',
+    './data/**/*.mdx',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: 'class',
