130f3e263c6cdb831cce5aff65dae92e978b9635
manika-signoz <manika@signoz.io>
2026-02-03 13:54:15 +0530

    fix: build

--- utils/strapi.ts
+++ utils/strapi.ts
@@ -340,11 +340,8 @@ export const fetchMDXContentByPath = async (
             tags: [`${collectionName}-list`],
           },
           headers: {
-            'Cache-Control': 'no-store', // Avoid caching
-            Pragma: 'no-cache',
-            Expires: '0',
             'Content-Type': 'application/json',
-            'Cache-Control': 'no-store',
+            'Cache-Control': 'no-store', // Avoid caching
             Pragma: 'no-cache',
             Expires: '0',
           },
